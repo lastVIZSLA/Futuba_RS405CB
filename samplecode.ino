cecc69377f842dbb5c2bb8009c52d9bceaef7dd4
Tarre Vizsla <42895412+lastVIZSLA@users.noreply.github.com>
2024-05-14 07:08:27 +0900

    Update samplecode.ino

--- samplecode.ino
+++ samplecode.ino
@@ -13,14 +13,14 @@ int motorID2 = 2;  // ID of the RS405CB motor
 
 void setup() {
 
-RS405CB myMotor( 9600, txdenPin, motorID1);
-RS405CB myMotor1( 9600, txdenPin, motorID2);
+RS405CB myMotor( 9600, txdenPin, motorID1); //use 115200 baud rate and motorid 1 is you are using motor for the first time
+RS405CB myMotor1( 9600, txdenPin, motorID2); //use 115200 baud rate and motorid 1 is you are using motor for the first time
 while(1){
-  myMotor.move(500, 100); // Move to position 1000 in 500 milliseconds
+  myMotor.move(500, 100); // Move to position 500 in 100 milliseconds
   delay(2000);
   myMotor.move(0, 100); 
   delay(2000);
-  myMotor1.move(500, 100); // Move to position 1000 in 500 milliseconds
+  myMotor1.move(500, 100); // Move to position 500 in 100 milliseconds
   delay(2000);
   myMotor1.move(0, 100); 
   delay(2000);
